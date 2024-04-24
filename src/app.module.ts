e2b79f5da4f485a3023b815346dbc1dac8c479fa
vu-pa <vu.pa@akb.vn>
2024-04-24 13:32:58 +0700

    fix typo

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppLoggerMiddleware } from './common/middlewares/Logger';
+import { AppLoggerMiddleware } from './common/middlewares/logger';
 import { GameModule } from './game/game.module';
 
 @Module({
